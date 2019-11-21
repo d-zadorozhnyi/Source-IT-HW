d7265af3f66eaad6222a5807be1a71e5d52544c0
Dima Zadorozhnyi <dimazador@gmail.com>
2019-11-21 13:07:41 +0200

    HW 2.1

---
+++ src/com/zadorozhnyi/Circumference.java
@@ -0,0 +1,16 @@
+package com.zadorozhnyi;
+
+import java.util.Scanner;
+
+public class Circumference {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Введите радиус окружности:");
+		double r = sc.nextDouble(); 
+		double l=Math.round((Math.PI*(Math.pow(r, 2))));
+		System.out.println("Площадь Треугольника = " +l);
+	}
+}
